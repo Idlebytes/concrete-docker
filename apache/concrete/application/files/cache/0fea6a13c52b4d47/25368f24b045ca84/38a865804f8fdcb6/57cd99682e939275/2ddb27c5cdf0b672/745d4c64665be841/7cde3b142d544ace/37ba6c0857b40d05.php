830ae5f67f88c7abb1320ac8181637a92f130606
Satheeshkumar Manoharan <ext-satheeshkumar.2.manoharan@microsoft.com>
2016-01-31 18:57:18 +0200

    Initial commit for concrete on docker

---
+++ apache/concrete/application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/7cde3b142d544ace/37ba6c0857b40d05.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_fedde9a6532a9937fe778440462029ae_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1454049737;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1453649275;
---
+++ mariadb/create-mariadb-user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+/usr/bin/mysqld_safe > /dev/null 2>&1 &
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    sleep 5
+    mysql -uroot -e "status" > /dev/null 2>&1
+    RET=$?
+done
+
+mysql -uroot -e "CREATE USER '$DEFAULT_USER'@'%' IDENTIFIED BY '$DEFAULT_PASS'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '$DEFAULT_USER'@'%' WITH GRANT OPTION"
+mysql -uroot -e "CREATE DATABASE cms"
+
+mysqladmin -uroot shutdown
